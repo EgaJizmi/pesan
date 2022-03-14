c587fda89401a25023323bbbd4589cbea8b888bb
EgaJizmi <kapoeram69@gmail.com>
2022-03-14 13:53:36 +0700

    comit 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,74 +1,78 @@
-<!DOCTYPE html>
-<!-- Created By CodingNepal -->
-<html lang="en" dir="ltr">
-   <head>
-     <link rel="stylesheet" href="/css/style.css">
+<head>
       <meta charset="utf-8">
       <title>Pesan Aja</title>
       <link rel="stylesheet" href="style.css">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
    </head>
    <body>
-      <div class="wrapper">
-         <div class="title-text">
-            <div class="title login">
-               pesan tiket
-            </div>
-           
-         </div>
-         <div class="form-container">
-       <label for="login" class="slide login">Data diri</label>
-            </div>
-            <div class="form-inner">
-               <form action="#" class="login">
-                  <div class="field">
-                     <input type="text" placeholder="Nama" required>
+      <div class="center">
+         <input type="checkbox" id="show">
+         <label for="show" class="show-btn">Pesan Disini</label>
+         <div class="container">
+            <label for="show" class="close-btn fas fa-times" title="close"></label>
+            <div class="text">
+               Pesan Aja
+            </div> <br>
+            <form action="#">
+               <label> <br> Asal Kota<label>
+                  <select class="form-select" aria-label="Default select example">
+                  <option selected>Malang</option>
+                  <option value="1">Pasuruan</option>
+                  <option value="2">Surabaya</option>
+                  <option value="3">solo</option>
+                  <option value="4">Jogja</option>
+                  <option value="5">Bandung</option>
+                  <option value="6">Jakarta</option>
+                  </select>
+               <label> Kota Tujuan<label>
+                  <select class="form-select" aria-label="Default select example">
+                  <option selected>Malang</option>
+                  <option value="1">Pasuruan</option>
+                  <option value="2">Surabaya</option>
+                  <option value="3">solo</option>
+                  <option value="4">Jogja</option>
+                  <option value="5">Bandung</option>
+                  <option value="6">Jakarta</option>
+                  </select> 
+                  
+                <div><label> <br>Tipe Kelas<label><div>  
+                     <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                     <label class="form-check-label" for="flexRadioDefault1">
+                        Premium
+                     </label>
+                     </div>
+                     <div class="form-check">
+                     <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+                     <label class="form-check-label" for="flexRadioDefault2">
+                        Eksekutif
+                     </label>
+                     </div>
+                     <div class="form-check">
+                     <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+                     <label class="form-check-label" for="flexRadioDefault2">
+                        Bisnis
+                     </label>
+                     </div>
+                     <div class="form-check">
+                     <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+                     <label class="form-check-label" for="flexRadioDefault2">
+                        Ekonomi
+                     </label>
                   </div>
-                  <div class="field">
-                     <input type="email" placeholder="Email Address" required>
-                  </div>
-                  <div class="field">
-                  <input type="tel" placeholder="Contact" pattern="[0-9]{4}-[0-9]-[0-9]-{4}" required >
-                  </div>
+                     <br>
+                     <div class="form-floating">                  
+                        <input type="date">
+                     </div>
+                     <div class="form-floating">                  
+                        <input type="date">
+                     </div>
                
-                  &ensp; 
-                  <div>
-                   </style>  mau kemana? 
-                  </div>
+               <div class="btn">
+                  <div class="inner"></div>
+                  <button type="submit">Pesan</button>
+               </div>
                
-                     <div class="field">
-                     <input type="text" placeholder="tujuan" required>
-                  </div>
-                  <div class="field">
-                     <input type="text" placeholder="asal" required>
-                  </div>
-                  <div class="field">
-                     <input type="date" placeholder="pilih kelas" required>
-                  </div>
-                  <div class="field">
-                     <input type="date"  required>
-                  </div>
-                  <div class="field btn">
-                     <div class="btn-layer"></div>
-                     <input type="submit" value="Pesan">
-                  </div>
-            
-            </div>
+            </form>
          </div>
       </div>
-      <script>
-         const loginText = document.querySelector(".title-text .login");
-         const loginForm = document.querySelector("form.login");
-         const loginBtn = document.querySelector("label.login");
-
-           loginForm.style.marginLeft = "-50%";
-           loginText.style.marginLeft = "-50%";
-         });
-         loginBtn.onclick = (()=>{
-           loginForm.style.marginLeft = "0%";
-           loginText.style.marginLeft = "0%";
-           return false;
-         });
-      </script>
    </body>
-</html>
